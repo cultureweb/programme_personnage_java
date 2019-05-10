8be50d81234481532ed68c9cfdee948f9cf97c64
ced <cedric.milius@yahoo.fr>
2019-05-10 12:04:27 +0200

    first commit

---
+++ src/Magician.java
@@ -0,0 +1,11 @@
+public class Magician {
+
+    String name;
+    String image;
+    int life;
+    int attack;
+    String sort;
+    String potion;
+
+}
+
---
+++ src/Warrior.java
@@ -0,0 +1,10 @@
+public class Warrior {
+
+        String name;
+        String image;
+        int life;
+        int attack;
+        String weapon;
+        String shield;
+
+}
