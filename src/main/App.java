39a1e3b0c4c1a30dc09ed5250a1565babbab7474
ced <cedric.milius@yahoo.fr>
2019-05-12 22:02:14 +0200

    create and set Character not the attack yet

--- src/main/App.java
+++ src/main/App.java
@@ -5,82 +5,84 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        //Création d'un objet warrior
-        Warrior warrior = new Warrior();
+        String typeOfCharacterToCreate = chooseTypeOfCharacterToCreate();
+        createCharacter(typeOfCharacterToCreate);
+
+    }
+
+    private static String chooseTypeOfCharacterToCreate() {
 
         Scanner sc = new Scanner(System.in);
 
         int choice;
+        String str;
         do {
-            System.out.println("Voullez vous créer un magicien ou un guerrier : tapez 1 pour Magicien et 2 pour Guerrier :");
+            System.out.println("Step-By-Step Choose your character 1-Magician 2-Warrior:");
             choice = sc.nextInt();
-//            System.out.println(choice);
         }
         while (choice != 1 && choice != 2);
 
-
-        System.out.println("Vous avez choisi : " + choice);
-
-        System.out.println("Veuillez saisir un nom de guerrier :");
-        String str = sc.nextLine();
-        System.out.println("Vous avez saisi : " + str);
-        warrior.setName(str);
-
-        System.out.println("Veuillez saisir l'adresse Url d'une image pour votre Guerrier " + warrior.getName());
-        str = sc.nextLine();
-        System.out.println("Vous avez saisi : " + str);
-        warrior.setImage(str);
-        int nbreSaisi;
-        do {
-            System.out.println("Veuillez saisir les points de vie de" + warrior.getName() + "entre 0 et 5");
-            nbreSaisi = sc.nextInt();
-
-        }
-        while (nbreSaisi < 0 || nbreSaisi > 5);
-
-        System.out.println("Vie = " + nbreSaisi);
-        warrior.setLife(nbreSaisi);
-
-//        System.out.println("Veuillez saisir les points de vie de"+ warrior.getName() + "entre 0 et 5");
-//        int num = sc.nextInt();
-//        System.out.println("Vous avez saisi : " + num);
-//        warrior.setLife(num);
-
-
-       /*System.out.println("Afficher les infos du Guerrier :");
-
-         duncan.setLife(5);
-         duncan.setAttack(3);
-         duncan.setWeapon("sword");
-         duncan.setShield("silver shied");
-
-        System.out.println("Name : " + duncan.getName());
-        System.out.println("Image : " + duncan.getImage());
-        System.out.println("Life :" + duncan.getLife());
-        System.out.println("Attack :" + duncan.getAttack());
-        System.out.println("Weapon :" + duncan.getWeapon());
-        System.out.println("Shield :" + duncan.getShield());
-
-
-        System.out.println("Afficher les infos du Magicien :");
-
-        Magician merlin = new Magician();
-        merlin.setName("Merlin");
-        merlin.setImage("http://ekladata.com/n4WZlH3YmOw5NW9gkUA8nSfWL_Y.png");
-        merlin.setLife(5);
-        merlin.setAttack(3);
-        merlin.setSort("fireBall");
-        merlin.setPotion("health");
-
-        System.out.println("Name : " + merlin.getName());
-        System.out.println("Image : " + merlin.getImage());
-        System.out.println("Life :" + merlin.getLife());
-        System.out.println("Attack :" + merlin.getAttack());
-        System.out.println("Weapon :" + merlin.getSort());
-        System.out.println("Shield :" + merlin.getPotion());*/
-
-
+        if (choice == 1) {
+            str = "magician";
+        } else str = "warrior";
+        return str;
     }
 
+    private static void createCharacter(String typeOfCharacterToCreate) {
+        int number;
 
+        switch (typeOfCharacterToCreate) {
+            case "warrior":
+                System.out.println("Choice: warrior");
+                Warrior warrior = new Warrior();
+                System.out.println("Please enter a " + typeOfCharacterToCreate + "'s name: ");
+                warrior.setName(recordStringDataFromConsole());
+                System.out.println("Name: " + warrior.getName());
+                System.out.println("Please enter an url link to set your image(avatar): ");
+                warrior.setImage(recordStringDataFromConsole());
+                System.out.println("Image: " + warrior.getImage());
+                do {
+                    System.out.println("Set Life level between 0 and 5: ");
+                    number = recordIntegerDataFromConsole();
+                }
+                while (number > 5 || number < 0);
+                warrior.setLife(number);
+                System.out.println("Life: " + warrior.getLife());
+                break;
+
+            case "magician":
+                System.out.println("Choice: magician");
+                Magician magician = new Magician();
+                System.out.println("Please enter a " + typeOfCharacterToCreate + "'s name: ");
+                magician.setName(recordStringDataFromConsole());
+                System.out.println("Name: " + magician.getName());
+                System.out.println("Please enter an url link to set your image(avatar): ");
+                magician.setImage(recordStringDataFromConsole());
+                System.out.println("image: " + magician.getImage());
+                do {
+                    System.out.println("Set Life level between 0 and 5: ");
+                    number = recordIntegerDataFromConsole();
+                }
+                while (number > 5 || number < 0);
+                magician.setLife(number);
+                System.out.println("Life: " + magician.getLife());
+                break;
+
+        }
+
+}
+
+    private static String recordStringDataFromConsole() {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+        return str;
+    }
+
+    private static Integer recordIntegerDataFromConsole() {
+        Scanner sc = new Scanner(System.in);
+        int numberEntered;
+        numberEntered = sc.nextInt();
+        return numberEntered;
+    }
 }
